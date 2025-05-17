555d9f94b8712833b8908f7a51cbbcebe8e0a251
wang <www.com>
2025-05-17 23:14:31 +0800

    add

---
+++ uniCloud-aliyun/cloudfunctions/getHome/index.js
@@ -0,0 +1,63 @@
+'use strict';
+exports.main = async (event, context) => {
+
+	try {
+
+		const data = {
+			projects: [{
+					id: 1,
+					title: '人人点评模板',
+					description: '匿名化点评平台，可做公司黑名单、外卖黑名单、等等',
+					image: 'https://env-00jxtfbgd5dl.normal.cloudstatic.cn/kescoy-banner1.jpg?expire_at=1747497128&er_sign=6cfe4c0d265a98808372d74a8b60b148',
+					tags: ['Vue3', 'uniapp'],
+					path: '/pages/comment/index/index'
+				},
+				{
+					id: 2,
+					title: '舞蹈店预约模板',
+					description: '舞蹈店预约模板，方便舞蹈店提供给用户预约功能',
+					image: 'https://env-00jxtfbgd5dl.normal.cloudstatic.cn/banner1.jpg?expire_at=1747497140&er_sign=d71b824588d4f3caae86d249ec7cc085',
+					tags: ['Vue3', 'uniapp'],
+					path: '/pages/dance/home/index'
+				},
+				{
+					id: 3,
+					title: '仿微信朋友圈模板',
+					description: '仿微信朋友圈模板，提供全方位的智能服务',
+					image: 'https://env-00jxtfbgd5dl.normal.cloudstatic.cn/微信图片_20241120114537.png?expire_at=1747497189&er_sign=208848dd037776c9a4fb75803f6e5f8a',
+					tags: ['Vue3', 'uniapp'],
+					path: '/pages/wx/home/index'
+				},
+				{
+					id: 4,
+					title: '门店模板',
+					description: '门店模板，方便门店提供给用户点单功能',
+					image: 'https://env-00jxtfbgd5dl.normal.cloudstatic.cn/微信图片_20241120131438.png?expire_at=1747497201&er_sign=4e3263fd9db54d9efe5f4ca09c4d4bf5',
+					tags: ['Vue3', 'uniapp'],
+					path: '/pages/shop/home/index'
+				}, {
+					id: 7,
+					title: '同城生活模板',
+					description: '同城生活模板，本地生活',
+					image: 'https://img1.baidu.com/it/u=2297377264,1459357291&fm=253&fmt=auto&app=138&f=JPG?w=265&h=380',
+					tags: ['Vue3', 'uniapp'],
+					path: '/pages/city/home/index'
+				}
+			],
+			wechatQr: 'https://objectstorageapi.bja.sealos.run/ppny09lg-uu-img/微信图片_20241120134411.png',
+			wechatQunQr: 'https://objectstorageapi.bja.sealos.run/ppny09lg-uu-img/23687d9f2f602d31317e1af279f1e0b.jpg'
+		}
+
+		return {
+			code: 0,
+			data: data,
+			message: 'success'
+		}
+	} catch (error) {
+		return {
+			code: -1,
+			data: null,
+			message: error.message
+		}
+	}
+}
---
+++ unpackage/dist/build/web/assets/uni-app.es.Dv4IFAzb.js
@@ -0,0 +1,1 @@
+import{ab as a,ac as s,ad as o,ae as r,af as t}from"./index-CrHV60vp.js";function n(a,s){return"string"==typeof a?s:a}const e=r=>(t,n=o())=>{!a&&s(r,t,n)},f=e(r),i=e(t);export{f as a,i as o,n as r};
